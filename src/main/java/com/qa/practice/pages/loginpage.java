6d8ce9f3d1240f983c8e5ae65ce8f2935d12614c
dilshad816 <deepadilshad8@gmail.com>
2022-09-09 13:19:18 -0500

    testNG

--- src/main/java/com/qa/practice/pages/loginpage.java
+++ src/main/java/com/qa/practice/pages/loginpage.java
@@ -1,5 +1,50 @@
 package com.qa.practice.pages;
 
-public class loginpage {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import com.qa.practice.base.TestBase;
+
+public class loginPage extends TestBase{
+
+  //Constructor  //page factory
+	public loginPage() {
+		
+		PageFactory.initElements(driver, this); //this reffer to local class instances
+		
+	}
+	
+	
+	@FindBy(xpath="//input[@name='email']")
+	WebElement userName;
+	
+	@FindBy(xpath="//input[@name='password']")
+	WebElement passWord;
+	
+	@FindBy(xpath="(//button[@type='submit'])[1]")
+	WebElement loginbutton;
+	
+	@FindBy(xpath="(//img[@alt='logo'])[1]")
+	WebElement logo;
+	
+	@FindBy(xpath="(//*[contains(@class,'icon-box icon')])[2]")
+	WebElement paymentLogo;
+	
+	public boolean phpLogo() {
+		return logo.isDisplayed();
+	}
+	
+	public boolean paymentLogo() {
+		return paymentLogo.isDisplayed();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
--- src/test/java/com/qa/practice/testcases/loginPageTEST.java
+++ src/test/java/com/qa/practice/testcases/loginPageTEST.java
@@ -1,5 +1,60 @@
 package com.qa.practice.testcases;
 
-public class loginPageTEST {
+import java.io.IOException;
 
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.practice.base.TestBase;
+import com.qa.practice.pages.loginPage;
+
+public class loginPageTEST extends TestBase {
+
+	loginPage lp ;
+	
+	
+	@BeforeMethod
+	public void SetUp() throws IOException {
+		
+		driver_run();
+		
+		lp = new loginPage();
+		
+	}	
+		
+	
+	
+	
+	
+	@Test
+	public void phpLogoTest() {
+		
+		Assert.assertTrue(lp.phpLogo());
+		
+	}
+	
+	
+	@Test
+	public void paymentLogoTest() {
+		
+		Assert.assertTrue(lp.paymentLogo());
+		
+		
+	}
+	
+	
+	
+	
+	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	
+	
+	
+	
 }
